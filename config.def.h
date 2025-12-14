4a476a444c6741a55d402c1714d3e0c7565d155f
Hugo SC <hugosc@users.noreply.github.com>
2025-12-14 14:28:06 +0000

    chore: move to local/src

--- config.def.h
+++ config.def.h
@@ -110,32 +110,32 @@ static const Layout layouts[] = {
 /* key definitions */
 #define MODKEY Mod4Mask // windows key
 #define TAGKEYS(KEY, TAG)                                                      \
-  {MODKEY, KEY, view, {.ui = 1 << TAG}},                                       \
-      {MODKEY | ControlMask, KEY, toggleview, {.ui = 1 << TAG}},               \
-      {MODKEY | ShiftMask, KEY, tag, {.ui = 1 << TAG}},                        \
-      {MODKEY | ControlMask | ShiftMask, KEY, toggletag, {.ui = 1 << TAG}},
+    {MODKEY, KEY, view, {.ui = 1 << TAG}},                                     \
+        {MODKEY | ControlMask, KEY, toggleview, {.ui = 1 << TAG}},             \
+        {MODKEY | ShiftMask, KEY, tag, {.ui = 1 << TAG}},                      \
+        {MODKEY | ControlMask | ShiftMask, KEY, toggletag, {.ui = 1 << TAG}},
 #define STACKKEYS(MOD, ACTION)                                                 \
-  {MOD, XK_j, ACTION##stack, {.i = INC(+1)}},                                  \
-      {MOD,                                                                    \
-       XK_k,                                                                   \
-       ACTION##stack,                                                          \
-       {.i = INC(-1)}}, /*{ MOD, XK_grave, ACTION##stack, {.i = PREVSEL } },   \
-                        { MOD, XK_q,     ACTION##stack, {.i = 0 } },           \
-                        { MOD, XK_a,     ACTION##stack, {.i = 1 } },           \
-                        { MOD, XK_z,     ACTION##stack, {.i = 2 } },           \
-                        { MOD, XK_x,     ACTION##stack, {.i = -1 } }, */
+    {MOD, XK_j, ACTION##stack, {.i = INC(+1)}},                                \
+        {MOD,                                                                  \
+         XK_k,                                                                 \
+         ACTION##stack,                                                        \
+         {.i = INC(-1)}}, /*{ MOD, XK_grave, ACTION##stack, {.i = PREVSEL } }, \
+                          { MOD, XK_q,     ACTION##stack, {.i = 0 } },         \
+                          { MOD, XK_a,     ACTION##stack, {.i = 1 } },         \
+                          { MOD, XK_z,     ACTION##stack, {.i = 2 } },         \
+                          { MOD, XK_x,     ACTION##stack, {.i = -1 } }, */
 
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd)                                                             \
-  {                                                                            \
-    .v = (const char *[]) { "/bin/sh", "-c", cmd, NULL }                       \
-  }
+    {                                                                          \
+        .v = (const char *[]) { "/bin/sh", "-c", cmd, NULL }                   \
+    }
 
 /* helper for launching gtk application */
 #define GTKCMD(cmd)                                                            \
-  {                                                                            \
-    .v = (const char *[]) { "/usr/bin/gtk-launch", cmd, NULL }                 \
-  }
+    {                                                                          \
+        .v = (const char *[]) { "/usr/bin/gtk-launch", cmd, NULL }             \
+    }
 
 #define STATUSBAR "dwmblocks"
 #define BROWSER "helium-browser"
